9a9e0cb2b9239fbb33f93baec2ac7acf91516bf8
BBBrando <31227156+BBBrando@users.noreply.github.com>
2020-04-26 20:39:09 -0700

    Update PracticeTest.cpp

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,6 +14,132 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
+//tests for numbersort
+
+TEST(PracticeTest, 321sort)
+{
+    Practice obj;
+    int first = 3;
+    int second = 2;
+    int third = 1;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+TEST(PracticeTest, 123sort)
+{
+    Practice obj;
+    int first = 1;
+    int second = 2;
+    int third = 3;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+TEST(PracticeTest, 312sort)
+{
+    Practice obj;
+    int first = 3;
+    int second = 1;
+    int third = 2;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+TEST(PracticeTest, 213sort)
+{
+    Practice obj;
+    int first = 2;
+    int second = 1;
+    int third = 3;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+TEST(PracticeTest, 231sort)
+{
+    Practice obj;
+    int first = 2;
+    int second = 3;
+    int third = 1;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+TEST(PracticeTest, 132sort)
+{
+    Practice obj;
+    int first = 1;
+    int second = 3;
+    int third = 2;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+TEST(PracticeTest, allNegSort)
+{
+    Practice obj;
+    int first = -3;
+    int second = -1;
+    int third = -2;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+TEST(PracticeTest, 111sort)
+{
+    Practice obj;
+    int first = 1;
+    int second = 1;
+    int third = 1;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+TEST(PracticeTest, 1negSort)
+{
+    Practice obj;
+    int first = -3;
+    int second = 1;
+    int third = 2;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 2 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+
+//commit3
+
+
+
+
+//tests for palindrome
 TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
